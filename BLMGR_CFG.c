cd923793a8aea18e81e63b4fa8ae0b2eed33d558
AliShawky07 <52102020+AliShawky07@users.noreply.github.com>
2019-09-06 07:28:13 +0200

    Add files via upload

---
+++ BLMGR_CFG.c
@@ -0,0 +1,12 @@
+﻿/*
+ * BLMGR_CFG.c
+ *
+ * Created: 28/02/2016 06:54:45 م
+ *  Author: hossam
+ */ 
+#include "DIO.h"
+#include "BLMGR_CFG.h"
+BLMGR_DioPinConfig BuzzerConfig      = {PC,1U<<4U};
+BLMGR_DioPinConfig BlueToothPwrConfig = {PC,1U<<5U};
+BLMGR_DioPinConfig BluetoothKeyConfig = {PD,1U<<2U};
+	
